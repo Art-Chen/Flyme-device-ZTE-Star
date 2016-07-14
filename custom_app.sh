47c5b4e7b49f853c551e33b1d3440b57b01644b0
Andy Chuck <1263972314@qq.com>
2016-07-14 12:36:57 +0800

    Add Romer info into FlymeSetting

--- custom_app.sh
+++ custom_app.sh
@@ -6,4 +6,9 @@ tempSmaliDir=$2
 if [ "$apkBaseName" = "SystemUI" ];then
         echo ">>> in custom_app for $apkBaseName to adjust the field IccCardConstants$State;->NETWORK_LOCKED with IccCardConstants$State;->PERSO_LOCKED"
         find $tempSmaliDir/ -name "*.smali" | xargs sed -i 's#IccCardConstants$State;->NETWORK_LOCKED#IccCardConstants$State;->PERSO_LOCKED#g'   
+
+elif [ "$apkBaseName" = "Settings" ];then 
+ 		echo ">>> in custom_app for $apkBaseName to add my Information in Setting/AboutPhone" 
+ 	sed -i '3a\    <Preference android:title="ROMer" android:summary="Art_Chen" style="?android:preferenceInformationStyle" />'  $tempSmaliDir/res/xml/device_info_settings.xml 
+
 fi
